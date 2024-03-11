3763c050a48096e42f50ad818bce61aaafae3adc
prashuchaudhary <prashu.chaudhary@capillarytech.com>
2024-03-11 13:28:29 +0530

    changes

--- magic_beans/beans/views.py
+++ magic_beans/beans/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 
 def index(request):
     return render(request, "beans/index.html", {})
+
+
+def magic(request):
+    query = request.POST.get("query")
+    return JsonResponse({'foo' : 'bar'})
