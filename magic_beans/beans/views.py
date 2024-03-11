bd497653b924bd9c60787c24c6af7855599adecc
glthrivikram <thrivikram.gl@capillarytech.com>
2024-03-11 14:50:31 +0530

    added openAI support

--- magic_beans/beans/views.py
+++ magic_beans/beans/views.py
@@ -1,5 +1,56 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.memory import ChatMessageHistory
+from typing import Dict
+from langchain_core.runnables import RunnablePassthrough
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain.chains.combine_documents import create_stuff_documents_chain
+import os
+
+
+def parse_retriever_input(params: Dict):
+    return params["messages"][-1].content
+
+
+module_dir = os.path.dirname(__file__) 
+file_path = os.path.join(module_dir, 'templates/beans/sample_vmm.txt') 
+
+
+chat = ChatOpenAI(model="gpt-4-0125-preview", temperature=0.7)
+loader = TextLoader(file_path)
+data = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+all_splits = text_splitter.split_documents(data)
+embeddings = OpenAIEmbeddings()
+db = FAISS.from_documents(all_splits, embeddings)
+retriever = db.as_retriever(k=4)
+
+question_answering_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "Answer the user's questions based on the below context:\n\n{context}",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+document_chain = create_stuff_documents_chain(chat, question_answering_prompt)
+
+
+chat_history = ChatMessageHistory()
+
+retrieval_chain_with_only_answer = (
+    RunnablePassthrough.assign(
+        context=parse_retriever_input | retriever,
+    )
+    | document_chain
+)
 
 
 def index(request):
@@ -7,6 +58,16 @@ def index(request):
 
 
 def magic(request):
-    query = request.POST.get("query")
-    return JsonResponse({'message': 'bar'})
+    global chat_history
+    global retrieval_chain_with_only_answer
+    query ='hello' #request.POST.get("query")
+    chat_history.add_user_message(query)
+    response=retrieval_chain_with_only_answer.invoke(
+        {
+            "messages": chat_history.messages,
+        }
+    )
+    chat_history.add_ai_message(response)
+    print(response)
+    return JsonResponse({'message': response})
 
